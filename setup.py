8e3fcf55d5b876efe8020512ec1131d83a3ee92c
mytja <mamnju21@tuta.io>
2021-03-06 20:14:28 +0100

    Added :tada: v1.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="AliExpressAPI", # Replace with your own username
-    version="beta1.0.0",
+    version="1.0.0",
     author="mytja",
     description="Get AliExpress results in a very very detailed JSON",
     long_description=long_description,
